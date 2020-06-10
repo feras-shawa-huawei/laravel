1072b5bb6ee092c03eaa626e579da279e617b4ba
FirasShawa <firas.alshawa@gmail.com>
2020-06-11 01:01:22 +0300

    modify models

--- app/Admin.php
+++ app/Admin.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notificatios\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatble;
 
-class Admin extends Model
+class Admin extends Authenticatble
 {
-    //
+    use Notifiable;
+
+    protected $gaurd = "admin";
+
+    protected $fillable = ['name','email','password'];
+
+    protected $hidden = ['password','remember_token'];
 }
--- app/Blogger.php
+++ app/Blogger.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notificatios\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatble;
 
-class Blogger extends Model
+class Blogger extends Authenticatble
 {
-    //
+    use Notifiable;
+
+    protected $gaurd = "blogger";
+
+    protected $fillable = ['name','email','password'];
+
+    protected $hidden = ['password','remember_token'];
 }
